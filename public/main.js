afcda4c096fab787ae88ea90ab2e3440508c4a01
Maria Garcia Puig <emailaddress@yahoo.com>
2021-12-15 17:42:58 +0100

    web v2

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 function login() {
-    var n = $('#usuari');
-    var p = $('#contrasenya');
+    alert("LOGIN");
+
+    var n = $('#username');
+    var p = $('#password');
 
     $.ajax({
         contentType: 'application/json',
@@ -10,7 +12,7 @@ function login() {
         url: "http://147.83.7.206:8080/dsaApp/user/login",
         success: function (result) {
             alert('USUARI I CONTRASENYA CORRECTES');
-            window.location.replace("principal.html");
+            window.location.replace("profile.html");
 
         }, error: function (result){alert('USUARI I/O CONTRASENYA INCORRECTES');
     }
@@ -18,24 +20,61 @@ function login() {
 }
 
 function register(){
+    alert("REGISTER");
+
     var n = $('#usuari');
     var p = $('#contrasenya');
+    var m = $('#mail');
 
     $.ajax({
         contentType: 'application/json',
         type: 'POST',
         dataType: 'json',
-        data: JSON.stringify({'name': n.val(), 'password': p.val()}),
+        data: JSON.stringify({'name': n.val(), 'password': p.val(), 'mail':m.val()}),
         url: "http://localhost:8080/dsaApp/user/add",
         success: function (result) {
             alert('USUARI REGISTRAT CORRECTAMENT');
-            window.location.replace("principal.html");
+            window.location.replace("profile.html");
 
         }, error: function (result){alert("EL NOM D'USUARI INTRODUIT JA EXISTEIX, PORVA AMB UN ALTRE DIFERENT");
     }
     })
 }
 
-function back(){
-    window.location.replace("index.html");
+function back(){ window.location.replace("index.html"); }
+
+function updateUser(){
+  alert("UPDATE");
+
+  var n = $('#usuari');
+  var p = $('#contrasenya');
+  var m = $('#mail');
+
+  $.ajax({
+      contentType: 'application/json',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify({'name': n.val(), 'password': p.val(), 'mail':m.val()}),
+      url: "http://localhost:8080/dsaApp/user/add",
+      success: function (result) {
+          alert('USUARI UPDATEJAT CORRECTAMENT');
+          window.location.replace("edituser.html");
+
+      }, error: function (result){alert("NO S'HA POGUT ACTUALITZAR CORRECTAMENT");
+  }
+  })
+
 }
+
+function deleteUser(){
+
+}
+
+
+
+
+function getInventary(){
+
+}
+
+function getUser(){}
