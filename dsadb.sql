03681b1644bd0f3bde62a9a6092b8d56b854c581
Miguel <miguel.mateos@estudiantat.upc.edu>
2022-01-06 05:08:49 +0100

    .gitignore updated

--- dsadb.sql
+++ dsadb.sql
@@ -5,7 +5,7 @@ USE dsadb;
 
 CREATE TABLE User(
 
-    id VARCHAR(30),
+	id VARCHAR(30),
 	name VARCHAR(30) PRIMARY KEY NOT NULL,
 	password VARCHAR(30),
 	mail VARCHAR(50),
@@ -19,17 +19,17 @@ CREATE TABLE Inventory(
 	userName VARCHAR(30),
 	itemName VARCHAR(30),
 	itemQuantity INT,
-    itemDescription VARCHAR(100),
-    itemAvatar VARCHAR(100)
-	
+	itemDescription VARCHAR(100),
+	itemAvatar VARCHAR(100)
+
 )ENGINE = InnoDB;
 
 CREATE TABLE Item(
 
 	name VARCHAR(30),
 	cost INT,
-    description VARCHAR(100),
-    avatar VARCHAR(100)
+	description VARCHAR(100),
+	avatar VARCHAR(100)
 
 )ENGINE = InnoDB;
 
@@ -52,6 +52,6 @@ CREATE TABLE Enemy(
 
 	name VARCHAR(30),
 	damage INT,
-    avatar VARCHAR(100)
+	avatar VARCHAR(100)
 
 )ENGINE = InnoDB;
