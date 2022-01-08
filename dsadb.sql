28bff518ccd6b75e24b3ad0aed43a58a332e274e
Miguel <miguel.mateos@estudiantat.upc.edu>
2022-01-08 19:00:51 +0100

    Enemy modified

--- dsadb.sql
+++ dsadb.sql
@@ -40,7 +40,6 @@ INSERT INTO Item (name, cost, description, avatar) values ("level3Item", 20, "It
 INSERT INTO Item (name, cost, description, avatar) values ("level4Item", 20, "Item to steal in the fourth level", "public/images/level4Item.jpeg");
 INSERT INTO Item (name, cost, description, avatar) values ("level5Item", 20, "Item to steal in the fifth level", "public/images/level5Item.jpeg");
 
-
 CREATE TABLE Game(
 
    	userName VARCHAR(30),
@@ -52,6 +51,10 @@ CREATE TABLE Enemy(
 
 	name VARCHAR(30),
 	damage INT,
+	description VARCHAR(100),
 	avatar VARCHAR(100)
 
 )ENGINE = InnoDB;
+
+INSERT INTO Enemy (name, damage, description, avatar) values ("securityGuard", 25, "As he sees you, he will not leave you alone", "public/images/securityGuard.jpeg");
+INSERT INTO Enemy (name, damage, description, avatar) values ("finalBoss", 100, "Stay away, it kills you with a slap", "public/images/finalBoss.jpeg");
--- src/main/java/edu/upc/dsa/models/Enemy.java
+++ src/main/java/edu/upc/dsa/models/Enemy.java
@@ -1,22 +1,26 @@
 package edu.upc.dsa.models;
 
 public class Enemy {
-    private String type;
+    private String name;
     private int damage;
+    private String description;
+    private String avatar;
 
     public Enemy(){}
 
-    public Enemy(String name, int damage) {
-        this.type = type;
+    public Enemy(String name, int damage, String description, String avatar) {
+        this.name = name;
         this.damage = damage;
+        this.description = description;
+        this.avatar = avatar;
     }
 
     public String getName() {
-        return type;
+        return name;
     }
 
-    public void setName(String type) {
-        this.type = type;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getDamage() {
@@ -25,5 +29,20 @@ public class Enemy {
 
     public void setDamage(int damage) {
         this.damage = damage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
 }
